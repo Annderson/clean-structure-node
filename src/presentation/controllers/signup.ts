de8ffdd875ba5cf1918340d646b5a8e1cc533776
Anderson Lima <anderson.rl.tr@gmail.com>
2021-06-27 20:11:32 -0300

    refactor(controllers): add new validation to the signup

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,18 +1,32 @@
-import { MissingParamError } from '../errors/missing-param-error';
-import { badRequest } from '../helpers/http-helper';
+import { InvalidParamError, MissingParamError } from '../errors';
+import { EmailValidator, badRequest, serverError } from '../helpers';
+
 import { HttpRequest, HttpResponse, Controller } from './../../types/http';
 
 const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator;
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
+      if (!this.emailValidator.isValid(httpRequest.body.email)) {
+        return badRequest(new InvalidParamError('email'));
+      }
+      return {
+        statusCode: 200,
+        body: 'sucesso'
+      }
+    } catch (error) {
+      return serverError()
     }
-    return {
-      statusCode: 200,
-      body: 'test'
-    };
   }
 }
