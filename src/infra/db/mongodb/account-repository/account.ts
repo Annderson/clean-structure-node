41185a69022cf1f7d37d5131f1c323344cd0ef25
Anderson Lima <anderson.lima-ext@olxbr.com>
2021-12-30 14:59:07 -0300

    feat(infra): create account class to talk to mongo

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,12 @@
+import { AddAccountRepository } from 'data/helpers/add-account-repository';
+import { AddAccountModel, AccountModel } from 'domain/models/account';
+import { MongoHelper } from '../helpers/mongo-helpers';
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts');
+    const result = await accountCollection.insertOne(accountData);
+    const account = await accountCollection.findOne({ _id: result.insertedId });
+    return MongoHelper.map(account);
+  }
+}
