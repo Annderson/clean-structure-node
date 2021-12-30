204948e3f40a2f6d8f7f9d209e7fb832bb1362de
Anderson Lima <anderson.lima-ext@olxbr.com>
2021-12-30 13:04:50 -0300

    feat(infra): add helpers to help mongo

---
+++ src/infra/db/mongodb/helpers/mongo-helpers.ts
@@ -0,0 +1,22 @@
+import { Collection, MongoClient } from 'mongodb';
+
+export const MongoHelper = {
+  client: null as unknown as MongoClient,
+  async connect (uri: string) {
+    this.client = await MongoClient.connect(process.env.MONGO_URL ?? '')
+  },
+  async disconnect (): Promise<void> {
+    await this.client.close();
+  },
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  },
+  map (collection: any): any {
+    const { _id, ...collectionResult } = collection;
+    return Object.assign(
+      {},
+      collectionResult,
+      { id: _id.toHexString() }
+    );
+  }
+};
