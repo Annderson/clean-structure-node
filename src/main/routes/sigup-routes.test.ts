9ddcd935191071ec43b5cd0734a2f80680b454ac
Anderson Lima <anderson.lima-ext@olxbr.com>
2022-01-21 10:46:33 -0300

    test(main): create SignUp route

---
+++ src/main/routes/sigup-routes.test.ts
@@ -0,0 +1,31 @@
+import request from 'supertest';
+
+import app from '../config/app';
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helpers';
+
+describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL ?? '')
+  });
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test('Should return an account on success ', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Anderson',
+        email: 'anderson@gamil.com',
+        password: '123',
+        passwordConfirmation: '123'
+      })
+      .expect(200)
+  })
+})
