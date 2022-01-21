0033e172c08dada40d379b2f748fbc7c864fa435
Anderson Lima <anderson.lima-ext@olxbr.com>
2022-01-21 10:06:24 -0300

    chore: add new configs to project

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/domain/**'
+  ],
   coverageDirectory: 'coverage',
   preset: '@shelf/jest-mongodb',
   testEnvironment: 'node',
